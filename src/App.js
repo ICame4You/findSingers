0a469f35eabe99301add1d92cf38c25a7bdd23a6
Alexander Shibaylo <alexshib91@gmail.com>
2018-12-04 08:32:46 +0300

    Find singers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,70 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+class SearchForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    const proxyurl = 'https://cors-anywhere.herokuapp.com/';
+    let searchResult = this.state.value.split(' ').join('+');
+    fetch(`${proxyurl}https://itunes.apple.com/search?term=${searchResult}`)
+    .then(this.handleErrors)
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      console.log(data.results);
+      return data;
+    })
+    .catch(error => console.log(error))
+  }
+
+  handleErrors(request) {
+    if(!request.ok) {
+      throw Error(request.status);
+    }
+    return request;
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+class OutputBlock extends Component {
+  render() {
+    return <h1>Output songs</h1>;
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <SearchForm />
+          <OutputBlock name='Alex' />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
